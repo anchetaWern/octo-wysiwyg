5e8e76527c04a709d73843941e47340277cf4692
Wern Ancheta <ancheta.wern@gmail.com>
2014-02-17 14:04:14 +0800

    add common shortcuts

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,41 @@
-function setSelectionRange(input, selectionStart, selectionEnd) {
-  if (input.setSelectionRange) {
-    input.focus();
-    input.setSelectionRange(selectionStart, selectionEnd);
-  }
-  else if (input.createTextRange) {
-    var range = input.createTextRange();
-    range.collapse(true);
-    range.moveEnd('character', selectionEnd);
-    range.moveStart('character', selectionStart);
-    range.select();
-  }
+function setSelectionRange(input, selectionStart, selectionEnd){
+	if(input.setSelectionRange){
+		input.focus();
+		input.setSelectionRange(selectionStart, selectionEnd);
+	}else if(input.createTextRange){
+		var range = input.createTextRange();
+		range.collapse(true);
+		range.moveEnd('character', selectionEnd);
+		range.moveStart('character', selectionStart);
+		range.select();
+	}
 }
 
-function setCaretToPos (input, pos) {
-  setSelectionRange(input, pos, pos);
+function setCaretToPos(input, pos){
+	setSelectionRange(input, pos, pos);
 }
+
+function getCaret(node){
+  if(node.selectionStart){
+    return node.selectionStart;
+  }else if(!document.selection){
+    return 0;
+  }
+
+  var c = "\001",
+      sel = document.selection.createRange(),
+      dul = sel.duplicate(),
+      len = 0;
+
+  dul.moveToElementText(node);
+  sel.text = c;
+  len = dul.text.indexOf(c);
+  sel.moveStart('character',-1);
+  sel.text = "";
+  return len;
+}
+
+$('.markdown-container').val($.trim($('.markdown-container').val()));
 
 $('.markdown-container').keyup(function(){
 	var markdown = $(this).val();
@@ -24,90 +45,95 @@ $('.markdown-container').keyup(function(){
 });
 
 $(document).bind('keydown', function(e){
-  var markdown = $('.markdown-container').val();
-  var current_text = $.trim($('.markdown-container').val());
-  var markdown_container = document.getElementsByClassName('markdown-container')[0];
+	var markdown = $('.markdown-container').val();
+	var current_text = $.trim($('.markdown-container').val());
+	var markdown_container = document.getElementsByClassName('markdown-container')[0];
 
-  if(e.ctrlKey && (e.which == 83)){
-    e.preventDefault();
-    $.post('/save_post', {'markdown' : markdown});
-    return false;
-  }
+	var cursor_position = getCaret(markdown_container);
+	var first_part = markdown.slice(0, cursor_position);
+	var second_part = markdown.slice(cursor_position, -1);
 
-  //insert headings
-  var keys = {'49' : '#', '50' : '##', '51' : '###', '52' : '####', '53' : '#####', '54' : '######'};
-  if(e.ctrlKey && (e.which >= 49 && e.which <= 54)){
-  	e.preventDefault();
-	$('.markdown-container').val(current_text + "\n" + keys[e.which]);
-
-  	return false;
-  }
-
-  //insert image
-  if(e.ctrlKey && e.which == 71){
-  	e.preventDefault();
-	
-  	var new_value = current_text + "![]()";
-	if(markdown_container.value.slice(-1) == "\n"){
-		new_value = current_text + "\n![]()"
+	//save post
+	if(e.ctrlKey && (e.which == 83)){
+		e.preventDefault();
+		$.post('/save_post', {'markdown' : markdown});
+		return false;
 	}
 
-	$('.markdown-container').val(new_value);  
-	setCaretToPos(markdown_container, markdown_container.selectionEnd - 1);
-	return false;
-  }
-
-  //bold
-  if(e.ctrlKey && e.which == 66){
-  	e.preventDefault();
-  	
-  	var new_value = current_text + " ****";
-  	if(markdown_container.value.slice(-1) == "\n"){
-		new_value = current_text + "\n****";
+	//insert headings
+	var keys = {'49' : '#', '50' : '##', '51' : '###', '52' : '####', '53' : '#####', '54' : '######'};
+	if(e.ctrlKey && (e.which >= 49 && e.which <= 54)){
+		e.preventDefault();
+		$('.markdown-container').val(first_part + "\n" + keys[e.which] + second_part);
+		setCaretToPos(markdown_container, cursor_position + keys[e.which].length + 1);
+		return false;
 	}
 
-	$('.markdown-container').val(new_value);  
-  	setCaretToPos(markdown_container, markdown_container.selectionEnd - 2);
-  	return false;
-  }
+	//insert image
+	if(e.ctrlKey && e.which == 71){
+		e.preventDefault();
 
-  //italic
-  if(e.ctrlKey && e.which == 73){
-  	e.preventDefault();
-  	
-  	var new_value = current_text + " **";
-   	if(markdown_container.value.slice(-1) == "\n"){
-		new_value = current_text + "\n**";
-	} 	
+		var image_value = first_part + " ![]()" + second_part;
+		if(markdown_container.value.slice(-1) == "\n"){
+			image_value = first_part + "\n![]()" + second_part;
+		}
 
-	$('.markdown-container').val(new_value);  
-	setCaretToPos(markdown_container, markdown_container.selectionEnd - 1);
-  	return false;
-  }
+		$('.markdown-container').val(image_value);
+		setCaretToPos(markdown_container, cursor_position + 5);
+		return false;
+	}
 
-  //blockquote
-  if(e.ctrlKey && e.which == 81){
-  	e.preventDefault();
-  	$('.markdown-container').val(current_text + "\n{% blockquote %}\n\n{% endblockquote %}"); 
-  	setCaretToPos(markdown_container, markdown_container.selectionEnd - 20);
-  	return false;
-  }
+	//bold
+	if(e.ctrlKey && e.which == 66){
+		e.preventDefault();
 
-  //more
-  if(e.which == 113){
-  	e.preventDefault();
-  	$('.markdown-container').val(current_text + "\n<!--more-->\n"); 
-  	
-  	return false;
-  }  
+		var bold_value = first_part + " ****" + second_part;
+		if(markdown_container.value.slice(-1) == "\n"){
+			bold_value = first_part + "\n****" + second_part;
+		}
 
-  //multi-line code
-  if(e.which == 112){
-  	e.preventDefault();	
-  	$('.markdown-container').val(current_text + "\n```\n```"); 
-  	setCaretToPos(markdown_container, markdown_container.selectionEnd - 4);
-  	return false;
-  }  
+		$('.markdown-container').val(bold_value);
+		setCaretToPos(markdown_container, cursor_position + 3);
+		return false;
+	}
+
+	//italic
+	if(e.ctrlKey && e.which == 73){
+		e.preventDefault();
+
+		var italic_value = first_part + " **" + second_part;
+			if(markdown_container.value.slice(-1) == "\n"){
+			italic_value = first_part + "\n**" + second_part;
+		}
+
+		$('.markdown-container').val(italic_value);
+		setCaretToPos(markdown_container, cursor_position + 2);
+		return false;
+	}
+
+	//blockquote
+	if(e.ctrlKey && e.which == 81){
+		e.preventDefault();
+		$('.markdown-container').val(first_part + "\n{% blockquote %}\n\n{% endblockquote %}" + second_part);
+		setCaretToPos(markdown_container, cursor_position + 20);
+		return false;
+	}
+
+	//more
+	if(e.which == 113){
+		e.preventDefault();
+		$('.markdown-container').val(first_part + "\n<!--more-->\n" + second_part);
+		setCaretToPos(markdown_container, cursor_position + 13);
+		return false;
+	}
+
+	//multi-line code
+	if(e.which == 112){
+		e.preventDefault();
+		$('.markdown-container').val(first_part + "\n```\n```" + second_part);
+		setCaretToPos(markdown_container, cursor_position + 4);
+		return false;
+	}
 
 });
 
@@ -122,14 +148,15 @@ $('#image_file').change(function(){
 	var file = image_file.files[0];
 	var filename = file.name;
 	reader.onload = function(e){
-		
-		$.post('/save_image', {'image_save_path' : image_save_path, 'data_uri' : reader.result, 'filename' : filename}, function(response){
-			var current_text = $.trim($('.markdown-container').val());
-			$('.markdown-container').val(current_text + $.trim(response));
+		$.post(
+			'/save_image',
+			{'image_save_path' : image_save_path, 'data_uri' : reader.result, 'filename' : filename},
+			function(response){
+				var current_text = $.trim($('.markdown-container').val());
+				$('.markdown-container').val(current_text + $.trim(response));
 		});
-		
-	}
+	};
 
-	reader.readAsDataURL(file); 
+	reader.readAsDataURL(file);
 });
 
