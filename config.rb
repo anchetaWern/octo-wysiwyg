d5cd152db13266a5a4e3361a1de46daf82da1d38
wern <ancheta.wern@gmail.com>
2015-01-10 09:44:24 +0800

    update config

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
-$root_dir = '/home/wern/web_files/octo-wysiwyg' #the directory in which octo-wysiwyg is installed
-$octopress_dir = '/home/wern/octopress'
+$root_dir = '/media/wern/Files/files/projects/octo-wysiwyg' #the directory in which octo-wysiwyg is installed
+$octopress_dir = '/media/wern/Files/files/octopress'
 $octopress_source_dir = $octopress_dir + '/source' #the source directory of your octopress installation
-$posts_dir = '/home/wern/octopress/source/_posts' #the _posts directory of your octopress installation
-$public_dir = '/home/wern/web_files/octo-wysiwyg/public' #the root directory for public assets
-$images_dir = '/images/posts' #the directory in which the post images will be uploaded
+$posts_dir = '/media/wern/Files/files/octopress/source/_posts' #the _posts directory of your octopress installation
+$public_dir = '/media/wern/Files/files/projects/octo-wysiwyg/public' #the root directory for public assets
+$images_dir = $octopress_dir + '/source/images/posts' #the directory in which the post images will be uploaded
